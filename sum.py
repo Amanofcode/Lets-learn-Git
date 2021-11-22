61aa629f3174b62b136d3134d28fd53d7e94e680
Aman <amanjain2691@gmail.com>
2021-11-22 14:11:02 +0530

    added message in sum.py

--- sum.py
+++ sum.py
@@ -2,4 +2,4 @@ a = int(input("Enter 1st number: "))
 b = int(input("Enter 2nd number: "))
 
 c = a + b
-print("The addition of the 2 numbers is",str(c))
+print("The addition of the above 2 numbers is",str(c))
